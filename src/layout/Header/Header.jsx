160e73df414e4dd5754fecce45c152cb161589f7
Yevhenii Dryha <dryga94@gmail.com>
2025-11-16 00:43:06 +0100

    fix logo

--- src/layout/Header/Header.jsx
+++ src/layout/Header/Header.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import { Navigation } from "../../components/Navigation/Navigation";
 import { Hamburger } from "../../components/Hamburger/Hamburger.jsx";
 import { useMedia } from "../../hooks/useMedia";
+import logo from "../../assets/svg/logo.svg"
 
 export const Header = () => {
   const { isMobile, isTablet } = useMedia();
@@ -10,7 +11,7 @@ export const Header = () => {
   return (
     <header className="header">
       <div className="header__container container">
-        <img className="header__logo" src="/logo.svg" alt="Logo" />
+        <img className="header__logo" src={logo} alt="Logo" />
         {isMobile ? <Hamburger /> : <Navigation />}
       </div>
     </header>
