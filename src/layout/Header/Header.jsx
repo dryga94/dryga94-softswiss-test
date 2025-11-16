e853ebe5f4f87a61cb28bb80cb1eca0bfabd130e
Yevhenii Dryha <dryga94@gmail.com>
2025-11-16 11:22:45 +0100

    polish index.html

--- src/layout/Header/Header.jsx
+++ src/layout/Header/Header.jsx
@@ -3,11 +3,11 @@ import React from "react";
 import { Navigation } from "../../components/Navigation/Navigation";
 import { Hamburger } from "../../components/Hamburger/Hamburger.jsx";
 import { useMedia } from "../../hooks/useMedia";
-import logo from "../../assets/svg/logo.svg"
+import logo from "../../assets/svg/logo.svg";
 
 export const Header = () => {
-  const { isMobile, isTablet } = useMedia();
-  console.log(isMobile, isTablet);
+  const { isMobile } = useMedia();
+
   return (
     <header className="header">
       <div className="header__container container">
